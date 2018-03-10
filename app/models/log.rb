3d7cab8703979fd3526ad28624ab384d79fad706
Raghu Betina <raghu@firstdraft.com>
2018-03-10 00:46:02 +0000

    Validate presence of logs.date

--- app/models/log.rb
+++ app/models/log.rb
@@ -5,6 +5,8 @@ class Log < ApplicationRecord
 
   # Validations
 
+  validates :date, :presence => { :message => "Select a valid date" }
+
   validates :rating_of_the_day, :presence => { :message => "How did you feel working on this habit today?" }
 
   validates :time_of_day, :presence => { :message => "When did you work on this habit today?" }
