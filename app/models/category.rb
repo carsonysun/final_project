90da6f5c5bf5508ac1f4b0e3d435b2a07f1c2094
Raghu Betina <raghu@firstdraft.com>
2018-03-10 00:46:02 +0000

    Validate uniqueness of categories.name

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,6 +5,8 @@ class Category < ApplicationRecord
 
   # Validations
 
+  validates :name, :uniqueness => { :message => "This category already exists!" }
+
   validates :name, :presence => { :message => "What habit category would you like to add?" }
 
   validates :name, :length => { :minimum => 1, :maximum => 25 }
