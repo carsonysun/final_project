697fb348404eddf246206671227245c26e60d698
Raghu Betina <raghu@firstdraft.com>
2018-03-10 00:46:24 +0000

    Whitelist columns in Habit for ActiveAdmin

--- app/admin/habits.rb
+++ app/admin/habits.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Habit do
+
+ permit_params :user_id, :name, :motivation, :category_id, :days_per_week
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
