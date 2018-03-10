2c552616be7115066ec585e025413bd4a7a85b84
Raghu Betina <raghu@firstdraft.com>
2018-03-10 00:46:21 +0000

    Whitelist columns in Log for ActiveAdmin

--- app/admin/logs.rb
+++ app/admin/logs.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Log do
+
+ permit_params :habit_id, :date, :time_of_day, :rating_of_the_day
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
