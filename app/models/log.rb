cfcef7693b8d29e3d634ad63103dac71eed28c89
Raghu Betina <raghu@firstdraft.com>
2018-03-10 00:46:02 +0000

    Validate presence of logs.rating_of_the_day

--- app/models/log.rb
+++ app/models/log.rb
@@ -5,6 +5,8 @@ class Log < ApplicationRecord
 
   # Validations
 
+  validates :rating_of_the_day, :presence => { :message => "How did you feel working on this habit today?" }
+
   validates :time_of_day, :presence => { :message => "When did you work on this habit today?" }
 
 end
